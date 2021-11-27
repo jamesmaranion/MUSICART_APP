cdb25ac9dd9df79093d2100d2fa8e5b2b4139e41
Cyrus Cabrera <Cyrus Cabrera@DESKTOP-0VPF7NL>
2021-11-27 22:56:05 +0800

    musicart

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace MUSICART_APP
 {
-    partial class Form1
+    partial class SongView
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,363 @@ namespace MUSICART_APP
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUploadSong = new System.Windows.Forms.Button();
+            this.txtSong = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtAlbum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSearchSong = new System.Windows.Forms.TextBox();
+            this.btnSearchSong = new System.Windows.Forms.Button();
+            this.Songg = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSongID = new System.Windows.Forms.Button();
+            this.BtnSearchAlbum = new System.Windows.Forms.Button();
+            this.txtAlbumTitle = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtAlbumID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSongID = new System.Windows.Forms.TextBox();
+            this.btnDeleteSong = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtSearchAlbumm = new System.Windows.Forms.TextBox();
+            this.btnSearchAlbum2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(55, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(681, 123);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnUploadSong
+            // 
+            this.btnUploadSong.Location = new System.Drawing.Point(443, 361);
+            this.btnUploadSong.Name = "btnUploadSong";
+            this.btnUploadSong.Size = new System.Drawing.Size(75, 23);
+            this.btnUploadSong.TabIndex = 2;
+            this.btnUploadSong.Text = "Upload Song";
+            this.btnUploadSong.UseVisualStyleBackColor = true;
+            this.btnUploadSong.Click += new System.EventHandler(this.btnUploadSong_Click);
+            // 
+            // txtSong
+            // 
+            this.txtSong.Location = new System.Drawing.Point(281, 247);
+            this.txtSong.Name = "txtSong";
+            this.txtSong.Size = new System.Drawing.Size(151, 20);
+            this.txtSong.TabIndex = 3;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(281, 286);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(151, 20);
+            this.txtPrice.TabIndex = 6;
+            // 
+            // txtAlbum
+            // 
+            this.txtAlbum.Location = new System.Drawing.Point(281, 321);
+            this.txtAlbum.Name = "txtAlbum";
+            this.txtAlbum.Size = new System.Drawing.Size(151, 20);
+            this.txtAlbum.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ReleaseDate";
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(281, 405);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(151, 20);
+            this.txtGenre.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Title";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(281, 209);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(151, 20);
+            this.txtTitle.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "SongList";
+            // 
+            // txtSearchSong
+            // 
+            this.txtSearchSong.Location = new System.Drawing.Point(89, 9);
+            this.txtSearchSong.Name = "txtSearchSong";
+            this.txtSearchSong.Size = new System.Drawing.Size(314, 20);
+            this.txtSearchSong.TabIndex = 18;
+            // 
+            // btnSearchSong
+            // 
+            this.btnSearchSong.Location = new System.Drawing.Point(443, 13);
+            this.btnSearchSong.Name = "btnSearchSong";
+            this.btnSearchSong.Size = new System.Drawing.Size(75, 23);
+            this.btnSearchSong.TabIndex = 19;
+            this.btnSearchSong.Text = "Search";
+            this.btnSearchSong.UseVisualStyleBackColor = true;
+            this.btnSearchSong.Click += new System.EventHandler(this.btnSearchSong_Click);
+            // 
+            // Songg
+            // 
+            this.Songg.AutoSize = true;
+            this.Songg.Location = new System.Drawing.Point(201, 247);
+            this.Songg.Name = "Songg";
+            this.Songg.Size = new System.Drawing.Size(32, 13);
+            this.Songg.TabIndex = 20;
+            this.Songg.Text = "Song";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(201, 324);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Album";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(201, 411);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Genre";
+            // 
+            // btnSongID
+            // 
+            this.btnSongID.Location = new System.Drawing.Point(558, 266);
+            this.btnSongID.Name = "btnSongID";
+            this.btnSongID.Size = new System.Drawing.Size(75, 23);
+            this.btnSongID.TabIndex = 24;
+            this.btnSongID.Text = "SongID";
+            this.btnSongID.UseVisualStyleBackColor = true;
+            this.btnSongID.Click += new System.EventHandler(this.btnSongID_Click);
+            // 
+            // BtnSearchAlbum
+            // 
+            this.BtnSearchAlbum.Location = new System.Drawing.Point(100, 286);
+            this.BtnSearchAlbum.Name = "BtnSearchAlbum";
+            this.BtnSearchAlbum.Size = new System.Drawing.Size(75, 23);
+            this.BtnSearchAlbum.TabIndex = 25;
+            this.BtnSearchAlbum.Text = "Album";
+            this.BtnSearchAlbum.UseVisualStyleBackColor = true;
+            this.BtnSearchAlbum.Click += new System.EventHandler(this.BtnSearchAlbum_Click);
+            // 
+            // txtAlbumTitle
+            // 
+            this.txtAlbumTitle.Location = new System.Drawing.Point(66, 247);
+            this.txtAlbumTitle.Name = "txtAlbumTitle";
+            this.txtAlbumTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtAlbumTitle.TabIndex = 26;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 204);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "AlbumID";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 250);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "AlbumTitle";
+            // 
+            // txtAlbumID
+            // 
+            this.txtAlbumID.Location = new System.Drawing.Point(66, 201);
+            this.txtAlbumID.Name = "txtAlbumID";
+            this.txtAlbumID.Size = new System.Drawing.Size(100, 20);
+            this.txtAlbumID.TabIndex = 29;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(496, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "SongID";
+            // 
+            // txtSongID
+            // 
+            this.txtSongID.Location = new System.Drawing.Point(558, 231);
+            this.txtSongID.Name = "txtSongID";
+            this.txtSongID.Size = new System.Drawing.Size(100, 20);
+            this.txtSongID.TabIndex = 16;
+            // 
+            // btnDeleteSong
+            // 
+            this.btnDeleteSong.Location = new System.Drawing.Point(639, 266);
+            this.btnDeleteSong.Name = "btnDeleteSong";
+            this.btnDeleteSong.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteSong.TabIndex = 30;
+            this.btnDeleteSong.Text = "Delete1";
+            this.btnDeleteSong.UseVisualStyleBackColor = true;
+            this.btnDeleteSong.Click += new System.EventHandler(this.btnDeleteSong_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 286);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 31;
+            this.btnDelete.Text = "delete2";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(24, 39);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 13);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "AlbumList";
+            // 
+            // txtSearchAlbumm
+            // 
+            this.txtSearchAlbumm.Location = new System.Drawing.Point(89, 39);
+            this.txtSearchAlbumm.Name = "txtSearchAlbumm";
+            this.txtSearchAlbumm.Size = new System.Drawing.Size(314, 20);
+            this.txtSearchAlbumm.TabIndex = 33;
+            // 
+            // btnSearchAlbum2
+            // 
+            this.btnSearchAlbum2.Location = new System.Drawing.Point(443, 43);
+            this.btnSearchAlbum2.Name = "btnSearchAlbum2";
+            this.btnSearchAlbum2.Size = new System.Drawing.Size(75, 23);
+            this.btnSearchAlbum2.TabIndex = 34;
+            this.btnSearchAlbum2.Text = "Search2";
+            this.btnSearchAlbum2.UseVisualStyleBackColor = true;
+            this.btnSearchAlbum2.Click += new System.EventHandler(this.btnSearchAlbum2_Click);
+            // 
+            // SongView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnSearchAlbum2);
+            this.Controls.Add(this.txtSearchAlbumm);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnDeleteSong);
+            this.Controls.Add(this.txtAlbumID);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtAlbumTitle);
+            this.Controls.Add(this.BtnSearchAlbum);
+            this.Controls.Add(this.btnSongID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Songg);
+            this.Controls.Add(this.btnSearchSong);
+            this.Controls.Add(this.txtSearchSong);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSongID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAlbum);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtSong);
+            this.Controls.Add(this.btnUploadSong);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "SongView";
+            this.Text = "SongView";
+            this.Load += new System.EventHandler(this.SongView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUploadSong;
+        private System.Windows.Forms.TextBox txtSong;
+    
+     
+        private System.Windows.Forms.TextBox txtPrice;
+      
+        private System.Windows.Forms.TextBox txtAlbum;
+        private System.Windows.Forms.Label label1;
+      
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSearchSong;
+        private System.Windows.Forms.Button btnSearchSong;
+        private System.Windows.Forms.Label Songg;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSongID;
+        private System.Windows.Forms.Button BtnSearchAlbum;
+        private System.Windows.Forms.TextBox txtAlbumTitle;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtAlbumID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSongID;
+        private System.Windows.Forms.Button btnDeleteSong;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtSearchAlbumm;
+        private System.Windows.Forms.Button btnSearchAlbum2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,90 @@ using System.Windows.Forms;
 
 namespace MUSICART_APP
 {
-    public partial class Form1 : Form
+    public partial class SongView : Form
     {
-        public Form1()
+        MusicArtEntities12 db;
+        public SongView()
         {
             InitializeComponent();
+            db = new MusicArtEntities12 ();
+            dataGridView1.DataSource = db.SongViews.ToList();
+           
+        }
+
+        private void btnUploadSong_Click(object sender, EventArgs e)
+        {   
+
+            int Album;
+            int.TryParse(txtAlbum.Text, out Album);
+            decimal Price;
+            decimal.TryParse(txtPrice.Text, out Price);
+            DateTime releaseDate = DateTime.Now;
+            var SongID = db.uspUploadSong(txtSong.Text,txtTitle.Text,Album,txtGenre.Text,releaseDate,Price);
+            if (SongID != null)
+            {
+                MessageBox.Show("New Song has been uploaded", "Upload Music", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView1.DataSource = db.uspGetSongs().ToList();
+            }
+           
+            
+        }
+
+        private void btnSearchSong_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.uspGetSongName(txtSearchSong.Text).ToList();
+        }
+
+
+        private void SongView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnSongID_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.uspGetSongID(txtSongID.Text).ToList();
+        }
+
+        private void BtnSearchAlbum_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.uspGetAlbumName(txtAlbumTitle.Text).ToList();
+            var AlbumID = db.uspUploadAlbum(txtAlbumTitle.Text);
+
+            if (AlbumID != null)
+            {
+                dataGridView1.DataSource = db.uspGetAlbum().ToList();
+            }
+        }
+
+        private void btnDeleteSong_Click(object sender, EventArgs e)
+        {
+            
+            var song = db.uspDeleteMusic(txtSongID.Text);
+
+            if(song!= null)
+            {
+                dataGridView1.DataSource = db.uspGetSongs().ToList();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var alb = db.uspDeleteAlbum(txtAlbumID.Text);
+            if (alb != null)
+            {
+                dataGridView1.DataSource = db.uspGetAlbum().ToList();
+            }
+        }
+
+        private void btnSearchAlbum2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.uspGetAlbumName(txtSearchAlbumm.Text).ToList();
         }
     }
 }
